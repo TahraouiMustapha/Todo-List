d44a55e043b2a0c17d06d1b7d587126427d59f64
mustapha <moustaphaoussama1818@gmail.com>
2024-04-28 08:18:36 +0100

    Add css, images loaders, style the header

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { compareAsc, format } from "date-fns";
+import './style.css';
 import { tasks } from './task.js';
 
 let todo = tasks.createTodos('tit', 'sdfuh',
                 format(new Date(2024, 4, 23), "yyyy-MM-dd"),
                 'important');
 
-                console.log(todo);
 
 
 
